97ab47fc373c9921d09200487ec544c03cc439fd
Admin <vanpho02@gmail.com>
2017-02-12 11:23:58 +0700

    end

---
+++ obj7.js
@@ -0,0 +1,9 @@
+let person = {
+  name: 'Khoa',
+  age: 18,
+  show: (who) => console.log(this.name + ' chao ban ' + who)
+ };
+
+// console.log(person);
+// let sh = person.show.bind(person)
+// sh('Pho');
