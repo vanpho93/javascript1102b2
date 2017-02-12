a4943d91213ea1199d2aa19118401bb678d338ad
Admin <vanpho02@gmail.com>
2017-02-12 11:26:52 +0700

    ket thuc

--- obj7.js
+++ obj7.js
@@ -1,9 +1,11 @@
 let person = {
   name: 'Khoa',
   age: 18,
-  show: (who) => console.log(this.name + ' chao ban ' + who)
+  show: function(who) {
+    console.log(this.name + ' chao ban ' + who)
+  }
  };
 
-// console.log(person);
-// let sh = person.show.bind(person)
-// sh('Pho');
+console.log(person);
+let sh = person.show.bind(person)
+sh('Pho');
