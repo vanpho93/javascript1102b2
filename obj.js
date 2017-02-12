57d0d8d1685b741bf812b62b33402d0fde155fc8
Admin <vanpho02@gmail.com>
2017-02-12 09:51:50 +0700

    phuong thuc

--- obj.js
+++ obj.js
@@ -5,8 +5,22 @@ let nhanVien1 = {name: 'Khoa', age: 18};
 function NhanVien (name, age){
   this.ten = name;
   this.tuoi = age;
+  this.xinChao = function(){
+    console.log('Xin chao, toi la ' + this.ten);
+  }
 }
 
-let Pho = new NhanVien('Pho', 20);
-console.log(Pho);
-console.log(Pho.ten);
+// let Pho = new NhanVien('Pho', 20);
+// console.log(Pho);
+// console.log(Pho.ten);
+// Pho.xinChao();
+
+function TamGiac(a, b, c){
+  this.canhA = a;
+  this.canhB = b;
+  this.canhC = c;
+  this.tinhChuVi = () => this.canhA + this.canhB + this.canhC;
+}
+
+let tg = new TamGiac(3, 4, 5);
+console.log(tg.tinhChuVi());
