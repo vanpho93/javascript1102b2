92b0810b0aace666ea126372f18d607a35cf69c5
Admin <vanpho02@gmail.com>
2017-02-12 10:50:54 +0700

    private

---
+++ obj5.js
@@ -0,0 +1,20 @@
+function Person(ten, tuoi, soDu) {
+  this.name = ten;
+  this.age = tuoi;
+  let soDuTaiKhoan = soDu;
+  this.getSoDu = function(){
+    return soDuTaiKhoan;
+  }
+  this.setSoDu = function(soDuMoi){
+    soDuTaiKhoan = soDuMoi
+  }
+}
+
+let ps = new Person ('Huong', 18, 1000);
+
+console.log(ps);
+console.log(ps.name);
+console.log(ps.age);
+console.log(ps.getSoDu());
+ps.setSoDu(1200);
+console.log(ps.getSoDu());
